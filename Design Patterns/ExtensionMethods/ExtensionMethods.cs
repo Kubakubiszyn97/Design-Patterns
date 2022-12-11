b700e4dcb597aa101aa52558c24a79907a6405aa
Kubakubiszyn97 <kubakubiszyn97@gmail.com>
2022-12-11 21:54:33 +0100

    Added Extension methods for serialization

---
+++ Design Patterns/ExtensionMethods/ExtensionMethods.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Design_Patterns.ExtensionMethods
+{
+    public static class ExtensionMethods
+    {
+        //Requires System.Serializable attribute
+        public static T DeepCopy<T>(this T self)
+        {
+            var stream = new MemoryStream();
+            var formater = new BinaryFormatter();
+            formater.Serialize(stream, self);
+            stream.Seek(0, SeekOrigin.Begin);
+            object copy = formater.Deserialize(stream);
+            stream.Close();
+            return (T)copy;
+        }
+
+        //Requires every class to have parametress constructor;
+        public static T DeepCopyXMl<T>(this T self)
+        {
+            using (var ms = new MemoryStream())
+            {
+                var s = new XmlSerializer(typeof(T));
+                s.Serialize(ms, self);
+                ms.Position = 0;
+                return (T) s.Deserialize(ms);
+            }
+        }
+    }
+}
