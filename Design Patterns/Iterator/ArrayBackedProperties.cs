d68dfa592dbb3866210e344c5a400fd7feb8ec79
Kuba Kubiszyn <jakubkubiszyn@wenklystudio.com>
2023-01-25 22:00:25 +0100

    Add array backed property example

---
+++ Design Patterns/Iterator/ArrayBackedProperties.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Design_Patterns.Iterator
+{
+    public class Creature : IEnumerable<int>
+    {
+        private int[] stats = new int[3];
+
+        private const int strength = 0;
+
+        public int Strength
+        {
+            get => stats[strength];
+            set => stats[strength] = value;
+        }
+
+        public int Agility { get; set; }
+        public int Intellligence { get; set; }
+
+        public int this[int index]
+        {
+            get { return stats[index]; }
+            set { stats[index] = value; }
+        }
+
+        public double AverageStat =>
+            stats.Average();
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            return stats.AsEnumerable().GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+           return GetEnumerator();
+        }
+    }
+}
